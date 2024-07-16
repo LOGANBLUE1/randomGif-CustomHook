a297b3f7041edb6da519cea4062dbc7ed36004d7
aditya <mrpareekaditya@gmail.com>
2024-07-16 17:03:05 +0530

    final updates

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,11 +1,6 @@
-import React, { useState } from 'react'
-import axios from 'axios';
-import { useEffect } from 'react';
+// import  from 'react'
 import Spinner from './Spinner';
 import useGif from '../hooks/useGif';
-
-
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 const Random = () => {
 
@@ -20,16 +15,14 @@ const Random = () => {
       <h1 className='mt-[15px] text-2xl underline uppercase font-bold'> A Random Gif</h1>
 
     {
-        loading ? (<Spinner/>) : (<img src= {gif} width="450" />)
+      loading ? (<Spinner/>) : (<img src= {gif} width="450" alt='it is'/>)
     }
 
       
 
       <button onClick={() => fetchData()}
-      className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]">
-
+       className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]">
        Generate
-
       </button>
 
     </div>
